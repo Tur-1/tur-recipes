b4617819f92e186c0ded2905a4825dd413d3bc23
Tur PC <tur.1i@hotmail.com>
2022-04-19 23:15:12 +0300

    added account page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="offcanvas offcanvas-bottom login-register-modal" tabindex="-1" id="login-modal"
+    <div class="offcanvas offcanvas-bottom login-register-modal" style="z-index: 99999999" tabindex="-1" id="login-modal"
         aria-labelledby="offcanvasBottomLabel">
         <div class="offcanvas-header">
             <span class="header-border"></span>
@@ -13,7 +13,7 @@
         </div>
     </div>
 
-    <div class="offcanvas offcanvas-bottom login-register-modal" tabindex="-1" id="register-modal"
+    <div class="offcanvas offcanvas-bottom login-register-modal" style="z-index: 99999999" tabindex="-1" id="register-modal"
         aria-labelledby="offcanvasBottomLabel">
         <div class="offcanvas-header">
             <span class="header-border"></span>
@@ -25,33 +25,26 @@
 
         </div>
     </div>
-    <div class="login-page">
 
-        <div class="row">
-            <div class="label">
-                <h1>Cooking a Delicious Food Easily </h1>
-                <p>discover more than 1200 food recipes in your hands and cooking it easily</p>
-            </div>
-            <div class="login-register-btns">
 
-                <button type="button" class="btn btn-primary" data-bs-toggle="offcanvas"
-                    data-bs-target="#login-modal">Login</button>
-                <button type="button" class="btn " data-bs-toggle="offcanvas" data-bs-target="#register-modal"
-                    id="register-btn">Sign
-                    up</button>
+    <div class="" id="login-page"
+        style="visibility: visible;background-image: url({{ asset('assets/images/loginBackgound.jpg') }})">
+
+        <div class="offcanvas-body ">
+            <div class="row">
+                <div class="label">
+                    <h1>Cooking a Delicious Food Easily </h1>
+                    <p>discover more than 1200 food recipes in your hands and cooking it easily</p>
+                </div>
+                <div class="login-register-btns">
+
+                    <button type="button" class="btn btn-primary" data-bs-toggle="offcanvas"
+                        data-bs-target="#login-modal">Login</button>
+                    <button type="button" class="btn " data-bs-toggle="offcanvas" data-bs-target="#register-modal"
+                        id="register-btn">Sign
+                        up</button>
+                </div>
             </div>
         </div>
-
-
     </div>
 @endsection
-
-@push('script')
-    <script>
-        document.body.style.backgroundImage = "url({{ asset('assets/images/loginBackgound.jpg') }})";
-        document.body.style.backgroundColor = "black";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "cover";
-        document.body.style.backgroundPosition = "initial";
-    </script>
-@endpush
--- resources/views/components/layouts/scripts.blade.php
+++ resources/views/components/layouts/scripts.blade.php
@@ -3,15 +3,11 @@
 
 <livewire:scripts />
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
-crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-</script>
+
+<script src="{{ asset('assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/jquery-migrate-3.3.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/bootstrap.bundle.js') }}"></script>
+
 
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="{{ asset('assets/js/main.js') }}"></script>
