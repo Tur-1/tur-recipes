84607af20c3e3d223a1be99d0a7dbb11e4c7a872
Tur HP <alhrabi.tur1@gmail.com>
2022-04-23 05:55:50 +0300

    fix all isssues

--- resources/views/components/home/recommend-recipes.blade.php
+++ resources/views/components/home/recommend-recipes.blade.php
@@ -1,8 +1,6 @@
 <div class="recommend-recipes">
-    <strong>Recommend recipes</strong>
+    <strong>Recommended recipes</strong>
     <div wire:ignore class="recommend-recipes-row">
-
-
         @foreach ($recommendRecipes as $recipe)
             <a role="button" href="#" class="card recipe-card"
                 wire:click.prevent='openRecipeModal({{ $recipe['id'] }})'>
@@ -29,27 +27,6 @@
             </a>
         @endforeach
     </div>
-    {{-- <a role="button" href="#" class="card" wire:click.prevent='openRecipeModal({{ $recipe['id'] }})'>
-        <div class="card-image-container">
-            <img src="{{ $recipe['image_url'] }}" alt="...">
-        </div>
-        <div class="card-body">
-            <div class="card-title">
-                <span>
-                    {{ $recipe['title'] }}
-                </span>
-            </div>
-            <div class="recipe-kcal-Time">
-                <div>
-                    <i class="fa-regular fa-clock me-1"></i>
-                    <span>{{ $recipe['ready_in_minutes'] }} </span>
-                </div>
-                <div>
-                    <i class="fa-solid fa-fire"></i>
-                    <span>{{ intval($recipe['calories']) }} </span>
-                </div>
-            </div>
-        </div>
-    </a> --}}
+
 
 </div>
