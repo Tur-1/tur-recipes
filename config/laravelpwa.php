3644aaac66e4cf6efd2bd7764b0378c7ea71d3ab
Tur HP <alhrabi.tur1@gmail.com>
2022-04-24 03:40:12 +0300

    last commit

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'name' => 'LaravelPWA',
+    'name' => 'T.Recipes',
     'manifest' => [
-        'name' => env('APP_NAME', 'TRecipes'),
-        'short_name' => 'PWA',
+        'name' => env('APP_NAME', 'T.Recipes'),
+        'short_name' => 'T.Recipes',
         'start_url' => '/',
-        'background_color' => '#ffffff',
+        'background_color' => '#000000',
         'theme_color' => '#000000',
         'display' => 'standalone',
         'orientation' => 'any',
@@ -46,16 +46,16 @@ return [
             ],
         ],
         'splash' => [
-            '640x1136' => '/images/icons/main.png',
-            '750x1334' => '/images/icons/splash-750x1334.png',
-            '828x1792' => '/images/icons/splash-828x1792.png',
-            '1125x2436' => '/images/icons/splash-1125x2436.png',
-            '1242x2208' => '/images/icons/splash-1242x2208.png',
-            '1242x2688' => '/images/icons/splash-1242x2688.png',
-            '1536x2048' => '/images/icons/splash-1536x2048.png',
-            '1668x2224' => '/images/icons/splash-1668x2224.png',
-            '1668x2388' => '/images/icons/splash-1668x2388.png',
-            '2048x2732' => '/images/icons/splash-2048x2732.png',
+            '640x1136' => '/images/icons/Screenshotsplash.png',
+            '750x1334' => '/images/icons/Screenshotsplash.png',
+            '828x1792' => '/images/icons/Screenshotsplash.png',
+            '1125x2436' => '/images/icons/Screenshotsplash.png',
+            '1242x2208' => '/images/icons/Screenshotsplash.png',
+            '1242x2688' => '/images/icons/Screenshotsplash.png',
+            '1536x2048' => '/images/icons/Screenshotsplash.png',
+            '1668x2224' => '/images/icons/Screenshotsplash.png',
+            '1668x2388' => '/images/icons/Screenshotsplash.png',
+            '2048x2732' => '/images/icons/Screenshotsplash.png',
         ],
         'shortcuts' => [
             [
