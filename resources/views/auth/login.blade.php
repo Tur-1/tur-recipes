8d6e46f69bd41f1964cb7e07f2e04e9e3a36bbe9
TUR PC <alhrabi.tur1@gmail.com>
2022-05-03 09:44:38 +0300

    convert website to pwa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,28 @@
 @extends('layouts.app')
 
 @section('body')
-    <div class="offcanvas offcanvas-bottom login-register-modal " id="login-modal" aria-labelledby="offcanvasBottomLabel">
-        <div class="offcanvas-header">
-            <span class="header-border"></span>
-        </div>
-        <div class="offcanvas-body small">
-            <x-auth.login-modal />
-        </div>
-    </div>
-
-    <div class="offcanvas offcanvas-bottom login-register-modal " id="register-modal" aria-labelledby="offcanvasBottomLabel">
-        <div class="offcanvas-header">
-            <span class="header-border"></span>
-
-
-        </div>
-        <div class="offcanvas-body small">
-            <x-auth.register-modal />
-
-        </div>
-    </div>
-
     <div class="login-page">
         <div class="login-page-header">
             <img src="{{ asset('assets/images/loginBackgound.jpg') }}" alt="">
         </div>
         <div class="login-page-body">
-            <div class="label">
-                <h2>Cooking a Delicious Food Easily </h2>
-                <p>discover more than 1200 food recipes in your hands and cooking it easily</p>
+            <div class="container">
+                <div class="label">
+                    <h2>Cooking a Delicious Food Easily </h2>
+                    <p>discover more than 1200 food recipes in your hands and cooking it easily</p>
+                </div>
+                <div class="login-register-btns">
+                    <button type="button" class="btn btn-primary" id="btn-login">Login</button>
+                    <button type="button" class="btn " id="btn-Sign-up"> Sign up </button>
+                </div>
             </div>
-            <div class="login-register-btns">
-                <button type="button" class="btn btn-primary" id="btn-login">Login</button>
-                <button type="button" class="btn " id="btn-Sign-up"> Sign up </button>
-            </div>
+
         </div>
+
     </div>
+
+    @include('components.auth.login-modal')
+    @include('components.auth.register-modal')
 @endsection
 
 
@@ -61,7 +47,7 @@
         $(document).ready(function() {
             $('.login-page').click(function(event) {
                 var $target = $(event.target);
-                if (!$target.closest('.login-page-body-container')
+                if (!$target.closest('.login-page')
                     .length) {
                     $('.login-register-modal').offcanvas('hide');
 
--- resources/views/components/auth/login-modal.blade.php
+++ resources/views/components/auth/login-modal.blade.php
@@ -1,39 +1,47 @@
- <form id="login-form " method="POST" action="{{ route('login') }}">
-     @csrf
+<div class="offcanvas offcanvas-bottom login-register-modal" id="login-modal" aria-labelledby="offcanvasBottomLabel">
+    <div class="offcanvas-header">
+        <span class="header-border"></span>
+    </div>
+    <div class="offcanvas-body small">
+        <form id="login-form " method="POST" action="{{ route('login') }}">
+            @csrf
 
-     <div class="form-group mb-2">
-         <label for="email" class=" col-form-label text-md-right">Email</label>
+            <div class="form-group mb-2">
+                <label for="email" class=" col-form-label text-md-right">Email</label>
 
-         <div class="">
-             <input id="email" type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
-                 name="email" value="{{ old('email') }}" autocomplete="email" placeholder="email">
+                <div class="">
+                    <input id="email" type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
+                        name="email" value="{{ old('email') }}" autocomplete="email" placeholder="email">
 
-             @if ($errors->has('email'))
-                 <span class="invalid-feedback" role="alert">
-                     <strong>{{ $errors->first('email') }}</strong>
-                 </span>
-             @endif
-         </div>
-     </div>
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
 
-     <div class="form-group mb-4">
+            <div class="form-group mb-4">
 
-         <label for="login_password" class=" col-form-label text-md-right">{{ __('Password') }}</label>
-         <input id="login_password" type="password" placeholder="password"
-             class="form-control  {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password">
+                <label for="login_password" class=" col-form-label text-md-right">{{ __('Password') }}</label>
+                <input id="login_password" type="password" placeholder="password"
+                    class="form-control  {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password">
 
-         @if ($errors->has('password'))
-             <span class="invalid-feedback" role="alert">
-                 <strong>{{ $errors->first('password') }}</strong>
-             </span>
-         @endif
-     </div>
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
 
 
-     <div data-form-target="#login-form"
-         class="userAuthHeadingTab  @if (count($errors->default) > 0) userAuthHeadingTab--invalid @endif">
-         <button type="submit" class="btn w-100" name="login">Sign
-             in</button>
-     </div>
+            <div data-form-target="#login-form"
+                class="userAuthHeadingTab  @if (count($errors->default) > 0) userAuthHeadingTab--invalid @endif">
+                <button type="submit" class="btn w-100" name="login">Sign
+                    in</button>
+            </div>
 
- </form>
+        </form>
+
+    </div>
+</div>
